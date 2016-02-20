e0fe217ed480706e4eb4aa1e001f4ed615dc43b7
halamidi <h.alamidi@yahoo.co.uk>
2016-02-20 21:33:08 +0000

    GPS working
    gps is working but needs to be refined

--- app/src/main/java/randomcompany/trackyourway/MapsActivity.java
+++ app/src/main/java/randomcompany/trackyourway/MapsActivity.java
@@ -1,7 +1,19 @@
 package randomcompany.trackyourway;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,17 +25,99 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private LocationManager locationMgr;
+    private LocationListener locationlist;
+    private double userLongitude;
+    private double userLatitude;
+    private Button btnFindMe;
+    // permissionsrequest not working right now
+    //permissionsRequest request = new permissionsRequest();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setview
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        btnFindMe = (Button) findViewById(R.id.button);
+        locationMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationlist = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                userLatitude = location.getLatitude();
+                userLongitude = location.getLongitude();
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent gpsSettings = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(gpsSettings);
+            }
+        };
+
+       checkPermission();
+       findme();
+        //time and distance updates are long because updates are not really required
+
+
     }
 
+    public void checkPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+                }, 1);
+                return;
+            }
+        } else {
+            findme();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                findme();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+    public void findme() {
+        btnFindMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationMgr.requestLocationUpdates("gps", 6000, 0, locationlist);
+                    LatLng user = new LatLng(userLatitude, userLongitude);
+                    mMap.addMarker(new MarkerOptions().position(user).title("user"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(user));
+                }else{
+                   checkPermission();
+                }
+
+
+            }
+        });
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -41,8 +135,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         LatLng nci = new LatLng(53.348889,-6.243199);
+
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.addMarker(new MarkerOptions().position(nci).title("Marker in nci"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(nci));
+       // mMap.addMarker(new MarkerOptions().position(user).title("user"));
+        //need to figure out how to ask and check for permissions
+        //if(checkPermission() = true) {
+            //mMap.setMyLocationEnabled(true);
+        //}
     }
+
 }
---
+++ app/src/main/java/randomcompany/trackyourway/permissionsRequest.java
@@ -0,0 +1,40 @@
+package randomcompany.trackyourway;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by Hassan on 09/02/2016.
+ */
+public class permissionsRequest extends MapsActivity {
+    //this will request users position and display on map when finished
+    public permissionsRequest(){
+
+
+    }
+//    public void checkPermission(){
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                requestPermissions(new String[]{
+//                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+//                }, 1);
+//                return;
+//            }
+//        } else {
+//            findme();
+//        }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        if (requestCode == 1) {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                findme();
+//            }
+//        } else {
+//            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        }
+//    }
+}

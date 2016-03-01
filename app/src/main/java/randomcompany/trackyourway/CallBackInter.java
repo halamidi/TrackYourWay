b927dc8a318e34bdfac3ae30539a0b0d5680f7e7
halamidi <h.alamidi@yahoo.co.uk>
2016-03-01 11:30:26 +0000

    fix

--- app/src/main/java/randomcompany/trackyourway/CallBackInter.java
+++ app/src/main/java/randomcompany/trackyourway/CallBackInter.java
@@ -1,7 +1,7 @@
-package randomcompany.trackyourway;
-
-/**
- * Created by Hassan on 29/02/2016.
- */
-interface CallBackInter {
-}
+//package randomcompany.trackyourway;
+//
+///**
+// * Created by Hassan on 29/02/2016.
+// */
+//interface CallBackInter {
+//}
--- app/src/main/java/randomcompany/trackyourway/LoginActivity.java
+++ app/src/main/java/randomcompany/trackyourway/LoginActivity.java
@@ -11,19 +11,19 @@ import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btnTest;
-    LocalUserDetails details;
+//    LocalUserDetails details;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         btnTest = (Button)findViewById(R.id.loginBtn);
-        details = new LocalUserDetails(this);
+       // details = new LocalUserDetails(this);
         //move outside of oncreate when it is working
         btnTest.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                UserAccount userLogin = new UserAccount(null,null);
-                details.storeDetails(userLogin);
-                details.isLoggedIn(true);
+               // UserAccount userLogin = new UserAccount(null,null);
+//                details.storeDetails(userLogin);
+//                details.isLoggedIn(true);
                 Intent i = new Intent(getApplicationContext(), MapsActivity.class);
                 startActivity(i);
 
@@ -32,19 +32,19 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
-    @Override
-    public void onStart(){
-        super.onStart();
-        if(authenticate() == true){
-            Intent i = new Intent(getApplicationContext(), MapsActivity.class);
-            startActivity(i);
-        }
+//    @Override
+//    public void onStart(){
+//        super.onStart();
+//        if(authenticate() == true){
+//            Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+//            startActivity(i);
+//        }
+//
+//    }
 
-    }
-
-    private boolean authenticate(){
-       return details.checkLoggedInstatus();
-    }
+//    private boolean authenticate(){
+//       return details.checkLoggedInstatus();
+//    }
 
 
 }

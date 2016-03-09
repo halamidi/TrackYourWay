9101661fde12031bdbd951a94faa380bb4e58bb6
EvanByrne <evanbyrne89@gmail.com>
2016-03-09 11:50:46 +0000

    Latest GUI updates

---
+++ app/src/main/java/randomcompany/trackyourway/Comments_Rating_Activity.java
@@ -0,0 +1,13 @@
+package randomcompany.trackyourway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Comments_Rating_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments__rating_);
+    }
+}

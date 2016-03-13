5b64566322e9faab194ff30d8b59ab46e74e5c50
halamidi <h.alamidi@yahoo.co.uk>
2016-03-13 15:34:42 +0000

    test commit

--- app/src/main/java/randomcompany/trackyourway/MainHub_Activity.java
+++ app/src/main/java/randomcompany/trackyourway/MainHub_Activity.java
@@ -1,30 +1,29 @@
 package randomcompany.trackyourway;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainHub_Activity extends AppCompatActivity {
-
+    ImageButton ForumBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_hub_);
+        ForumBtn = (ImageButton)findViewById(R.id.ForumBtn);
+
+
+        ForumBtn.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                                Intent i = new Intent(getApplicationContext(), Comments_Rating_Activity.class);
+                                startActivity(i);
+                            }
+
+                            });
     }
 
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.savedSearchBtn:
 
-                break;
-            case R.id.newSearchBtn:
-                break;
-            case R.id.ForumBtn:
-                Intent i = new Intent(getApplicationContext(), Comments_Rating_Activity.class);
-                startActivity(i);
-                break;
-        }
 
-    }
 }

87c84e357632043de4d146b94e323f4d21d881fb
PaddyK95 <PaddyK95@hotmail.com>
2016-04-10 22:43:52 +0100

    Comments_rating Activity with nav drawer

--- app/src/main/java/randomcompany/trackyourway/Comments_Rating_Activity.java
+++ app/src/main/java/randomcompany/trackyourway/Comments_Rating_Activity.java
@@ -1,7 +1,15 @@
 package randomcompany.trackyourway;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -12,7 +20,13 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Comments_Rating_Activity extends AppCompatActivity {
+public class Comments_Rating_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //creating objects for navigation drawer
+    DrawerLayout mdrawer;
+    NavigationView mNavigationView;
+    Toolbar toolbar;
+
 
     private List<Ratings> myRatings = new ArrayList<Ratings>();
 
@@ -24,7 +38,61 @@ public class Comments_Rating_Activity extends AppCompatActivity {
 
         populateRatingsList();
         populateListView();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //hamburger icon to open navigation drawer
+        mdrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mdrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mdrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //creating nav view with items in it
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
+
     }
+
+
+    // opens drawer when going back to activity
+    @Override
+    public void onBackPressed() {
+        mdrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (mdrawer.isDrawerOpen(GravityCompat.START)) {
+            mdrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
 
     private void populateRatingsList() {
         myRatings.add(new Ratings("Brilliant", R.drawable.stars4));
@@ -66,5 +134,52 @@ public class Comments_Rating_Activity extends AppCompatActivity {
         }
 
     }
+
+    //functionality to open activities in nav drawer
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handles the navigation drawer items stored
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            //Handles the click event for the home activity
+            Intent intent = new Intent(this, MainHub_Activity.class);
+            startActivity(intent);
+            return true;
+
+        } else if (id == R.id.nav_new_search) {
+            //Handles the click event for the search activity
+            Intent intent = new Intent(this, SearchFragment.class);
+            startActivity(intent);
+            return true;
+
+            //handles the click even for the saved search activity
+        } else if (id == R.id.nav_saved_searches) {
+            Intent intent = new Intent(this, Saved_Results_Activity.class);
+            startActivity(intent);
+            return true;
+
+            //handles the click even for the forum activity
+        } else if (id == R.id.nav_forum) {
+            Intent intent = new Intent(this, Comments_Rating_Activity.class);
+            startActivity(intent);
+            return true;
+
+            //handles the click even for the user etails activity
+        } else if (id == R.id.nav_user_details) {
+            Intent intent = new Intent(this, MainHub_Activity.class);
+            startActivity(intent);
+            return true;
+
+        }
+
+        mdrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mdrawer.closeDrawer(GravityCompat.START);
+        return true;
+
+
+    }
+
 
 }

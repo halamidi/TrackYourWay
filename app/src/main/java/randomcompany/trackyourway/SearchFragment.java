5ba475c1eaf708914941f20993fddaa640b2e6d7
halamidi <h.alamidi@yahoo.co.uk>
2016-04-12 07:58:57 +0100

    fix 3

--- app/src/main/java/randomcompany/trackyourway/SearchFragment.java
+++ app/src/main/java/randomcompany/trackyourway/SearchFragment.java
@@ -61,12 +61,76 @@ public class SearchFragment extends AppCompatActivity implements NavigationView.
         //buttons on the layout to open other activities
         SearchBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent i;
-                i = new Intent(getApplicationContext(), Results_Activity.class);
-                startActivity(i);
+              SearchPeramiters search = new SearchPeramiters();
+ -                storeDbresults storeParam = new storeDbresults();
+ -                CollegeName = instituteTf.getText().toString();
+ -                CourseName = courseTf.getText().toString();
+ -                CollegeLocation = locationTf.getText().toString();
+ -                CourseType = courseTypeTf.getText().toString();
+ -                keywords = courseTf.getText().toString();
+ -                //checking for every possible outcome
+ -//                if(!CollegeName.equals(null) && !CollegeName.equals("")){
+ -//                    //keywords will not be nested in any section
+ -//                    search.setCollegeName(CollegeName);
+ -//                    if(!CourseName.equals(null) && !CourseName.equals("")){
+ -//                        search.setCourseName(CourseName);
+ -//                    }else if(!CollegeLocation.equals(null) && !CollegeLocation.equals("")){
+ -//                        search.setCollegeLocation(CollegeLocation);
+ -//                    }else  if(!CourseType.equals(null) && !CourseType.equals("")){
+ -//                        search.setCourseType(CourseType);
+ -//                    }
+ -//                }else  if(!CourseName.equals(null) && !CourseName.equals("")){
+ -//                    search.setCourseName(CourseName);
+ -//                    if(!CollegeLocation.equals(null) && !CollegeLocation.equals("")){
+ -//                        search.setCollegeLocation(CollegeLocation);
+ -//                    }else  if(!CourseType.equals(null) && !CourseType.equals("")){
+ -//                        search.setCourseType(CourseType);
+ -//                    }
+ -//                }else  if(!CollegeLocation.equals(null) && !CollegeLocation.equals("")){
+ -//                    search.setCollegeLocation(CollegeLocation);
+ -//                    if(!CourseType.equals(null) && !CourseType.equals("")){
+ -//                        search.setCourseType(CourseType);
+ -//                    }
+ -//                }else  if(!CourseType.equals(null) && !CourseType.equals("")){
+ -//                    search.setCourseType(CourseType);
+ -//                }else  if(!keywords.equals(null) && !keywords.equals("")){
+ -//                    search.setKeyWords(keywords);
+ -//                }else{
+ -//                    //will change to ouput on screen when i have everything else working
+ -//                    Log.d("you have not ", "entered any details");
+ -//                }
+ -                //set variable
+ -                if (CollegeName.equals(null)) {
+ -                    Log.d("you have not ", "entered any details");
+ -                }else {
+ -                    storeParam.setUserSearch(CollegeName);
+ -                    CheckDetails(storeParam);
+ -                }
+ -//                Intent i;
+ -//                i = new Intent(getApplicationContext(), Results_Activity.class);
+ -//                startActivity(i);
             }
         });
     }
+    
+    private void CheckDetails(storeDbresults newSearchParam){
+ -        //send data to database request
+ -        String Type = "Search";
+ -        DbRequest newRequest = new DbRequest(this);
+ -        newRequest.DbRetrieveDetails(Type, newSearchParam, new CallBackInter() {
+ -            @Override
+ -            public void complete(storeDbresults newObject) {
+                 storeDbresults searchPerams = newObject;
+ -                //check if correct
+ -                if (searchPerams == null/*userLogin.checkEmpty() == true*/) {
+ -                    Log.d(null, "something has gone wrong");
+ -                    //Warninglbl.setText("user details were incorrect");
+ -                } else {
+ -                    Log.d("testing object", searchPerams.getCollegeDetails().CollegeName);
+  
+ -                }
+ -            }
+ -        });
 
     // opens drawer when going back to previous activity
     @Override

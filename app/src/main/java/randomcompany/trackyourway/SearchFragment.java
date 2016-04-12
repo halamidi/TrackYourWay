97ee91edb3d1103789fd1b0a705ebfc73776e8ea
halamidi <h.alamidi@yahoo.co.uk>
2016-04-12 12:35:20 +0100

    hysabdhyduhqyw

--- app/src/main/java/randomcompany/trackyourway/SearchFragment.java
+++ app/src/main/java/randomcompany/trackyourway/SearchFragment.java
@@ -10,7 +10,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,9 +21,6 @@ import android.widget.ImageButton;
  * A simple {@link Fragment} subclass.
  */
 public class SearchFragment extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-
-    public String CollegeName, CourseName, CollegeLocation, CourseType, keywords;
-
 
     public EditText instituteTf;
     public EditText courseTf;
@@ -65,158 +61,157 @@ public class SearchFragment extends AppCompatActivity implements NavigationView.
         //buttons on the layout to open other activities
         SearchBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                SearchPeramiters search = new SearchPeramiters();
-                storeDbresults storeParam = new storeDbresults();
-                CollegeName = instituteTf.getText().toString();
-                CourseName = courseTf.getText().toString();
-                CollegeLocation = locationTf.getText().toString();
-                CourseType = courseTypeTf.getText().toString();
-                keywords = courseTf.getText().toString();
-                //checking for every possible outcome
-                //                if(!CollegeName.equals(null) && !CollegeName.equals("")){
-                //                    //keywords will not be nested in any section
-                //                    search.setCollegeName(CollegeName);
-                //                    if(!CourseName.equals(null) && !CourseName.equals("")){
-                //                        search.setCourseName(CourseName);
-                //                    }else if(!CollegeLocation.equals(null) && !CollegeLocation.equals("")){
-                //                        search.setCollegeLocation(CollegeLocation);
-                //                    }else  if(!CourseType.equals(null) && !CourseType.equals("")){
-                //                        search.setCourseType(CourseType);
-                //                    }//                }else  if(!CourseName.equals(null) && !CourseName.equals("")){
-                //                    search.setCourseName(CourseName);
-                //                    if(!CollegeLocation.equals(null) && !CollegeLocation.equals("")){
-                //                        search.setCollegeLocation(CollegeLocation);
-                //                    }else  if(!CourseType.equals(null) && !CourseType.equals("")){
-                //                        search.setCourseType(CourseType);
-                //                    }
-                //                }else  if(!CollegeLocation.equals(null) && !CollegeLocation.equals("")){
-                //                    search.setCollegeLocation(CollegeLocation);
-                //                    if(!CourseType.equals(null) && !CourseType.equals("")){
-                //                        search.setCourseType(CourseType);
-                //                    }
-                //                }else  if(!CourseType.equals(null) && !CourseType.equals("")){
-                //                    search.setCourseType(CourseType);
-                //                }else  if(!keywords.equals(null) && !keywords.equals("")){
-                //                    search.setKeyWords(keywords);
-                //                }else{
-                //                    //will change to ouput on screen when i have everything else working
-                //                    Log.d("you have not ", "entered any details");
-                //                }
-                //set variable
-                if (CollegeName.equals(null)) {
-                    Log.d("you have not ", "entered any details");
-                } else {
-                    storeParam.setUserSearch(CollegeName);
-                    CheckDetails(storeParam);
-                }
-                //                Intent i;
-                //                i = new Intent(getApplicationContext(), Results_Activity.class);
-                //                startActivity(i);}
+              SearchPeramiters search = new SearchPeramiters();
+ -                storeDbresults storeParam = new storeDbresults();
+ -                CollegeName = instituteTf.getText().toString();
+ -                CourseName = courseTf.getText().toString();
+ -                CollegeLocation = locationTf.getText().toString();
+ -                CourseType = courseTypeTf.getText().toString();
+ -                keywords = courseTf.getText().toString();
+ -                //checking for every possible outcome
+ -//                if(!CollegeName.equals(null) && !CollegeName.equals("")){
+ -//                    //keywords will not be nested in any section
+ -//                    search.setCollegeName(CollegeName);
+ -//                    if(!CourseName.equals(null) && !CourseName.equals("")){
+ -//                        search.setCourseName(CourseName);
+ -//                    }else if(!CollegeLocation.equals(null) && !CollegeLocation.equals("")){
+ -//                        search.setCollegeLocation(CollegeLocation);
+ -//                    }else  if(!CourseType.equals(null) && !CourseType.equals("")){
+ -//                        search.setCourseType(CourseType);
+ -//                    }
+ -//                }else  if(!CourseName.equals(null) && !CourseName.equals("")){
+ -//                    search.setCourseName(CourseName);
+ -//                    if(!CollegeLocation.equals(null) && !CollegeLocation.equals("")){
+ -//                        search.setCollegeLocation(CollegeLocation);
+ -//                    }else  if(!CourseType.equals(null) && !CourseType.equals("")){
+ -//                        search.setCourseType(CourseType);
+ -//                    }
+ -//                }else  if(!CollegeLocation.equals(null) && !CollegeLocation.equals("")){
+ -//                    search.setCollegeLocation(CollegeLocation);
+ -//                    if(!CourseType.equals(null) && !CourseType.equals("")){
+ -//                        search.setCourseType(CourseType);
+ -//                    }
+ -//                }else  if(!CourseType.equals(null) && !CourseType.equals("")){
+ -//                    search.setCourseType(CourseType);
+ -//                }else  if(!keywords.equals(null) && !keywords.equals("")){
+ -//                    search.setKeyWords(keywords);
+ -//                }else{
+ -//                    //will change to ouput on screen when i have everything else working
+ -//                    Log.d("you have not ", "entered any details");
+ -//                }
+ -                //set variable
+ -                if (CollegeName.equals(null)) {
+ -                    Log.d("you have not ", "entered any details");
+ -                }else {
+ -                    storeParam.setUserSearch(CollegeName);
+ -                    CheckDetails(storeParam);
+ -                }
+ -//                Intent i;
+ -//                i = new Intent(getApplicationContext(), Results_Activity.class);
+ -//                startActivity(i);
             }
         });
     }
-
-
     
-    private void CheckDetails(storeDbresults newSearchParam) {
-        //send data to database request
-        String Type = "Search";
-        DbRequest newRequest = new DbRequest(this);
-        newRequest.DbRetrieveDetails(Type, newSearchParam, new CallBackInter() {
-            @Override
-            public void complete(storeDbresults newObject) {
-                storeDbresults searchPerams = newObject;
-                //check if correct
-                if (searchPerams == null/*userLogin.checkEmpty() == true*/) {
-                    Log.d(null, "something has gone wrong");
-                    //Warninglbl.setText("user details were incorrect");
-                } else {
-                    Log.d("testing object", searchPerams.getCollegeDetails().CollegeName);
+    private void CheckDetails(storeDbresults newSearchParam){
+ -        //send data to database request
+ -        String Type = "Search";
+ -        DbRequest newRequest = new DbRequest(this);
+ -        newRequest.DbRetrieveDetails(Type, newSearchParam, new CallBackInter() {
+ -            @Override
+ -            public void complete(storeDbresults newObject) {
+                 storeDbresults searchPerams = newObject;
+ -                //check if correct
+ -                if (searchPerams == null/*userLogin.checkEmpty() == true*/) {
+ -                    Log.d(null, "something has gone wrong");
+ -                    //Warninglbl.setText("user details were incorrect");
+ -                } else {
+ -                    Log.d("testing object", searchPerams.getCollegeDetails().CollegeName);
+  
+ -                }
+ -            }
+ -        });
 
-                }
-            }
-        });
-    }
-
-        // opens drawer when going back to previous activity
-        @Override
-        public void onBackPressed () {
-            mdrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            if (mdrawer.isDrawerOpen(GravityCompat.START)) {
-                mdrawer.closeDrawer(GravityCompat.START);
-            } else {
-                super.onBackPressed();
-            }
-        }
-
-        @Override
-        public boolean onCreateOptionsMenu (Menu menu){
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.main, menu);
-            return true;
-        }
-
-
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
-
-            return super.onOptionsItemSelected(item);
-        }
-
-        //functionality to open activities in nav drawer
-        @SuppressWarnings("StatementWithEmptyBody")
-        @Override
-        public boolean onNavigationItemSelected (MenuItem item){
-            // Handle navigation view item clicks here.
-            int id = item.getItemId();
-
-            if (id == R.id.nav_home) {
-                //Handles the click event for the home activity
-                Intent intent = new Intent(this, MainHub_Activity.class);
-                startActivity(intent);
-                return true;
-
-            } else if (id == R.id.nav_new_search) {
-                //Handles the click event for the search activity
-                Intent intent = new Intent(this, SearchFragment.class);
-                startActivity(intent);
-                return true;
-
-                //handles the click even for the saved search activity
-            } else if (id == R.id.nav_saved_searches) {
-                Intent intent = new Intent(this, Saved_Results_Activity.class);
-                startActivity(intent);
-                return true;
-
-                //handles the click even for the forum activity
-            } else if (id == R.id.nav_forum) {
-                Intent intent = new Intent(this, Comments_Rating_Activity.class);
-                startActivity(intent);
-                return true;
-
-                //handles the click even for the user etails activity
-            } else if (id == R.id.nav_user_details) {
-                Intent intent = new Intent(this, MainHub_Activity.class);
-                startActivity(intent);
-                return true;
-
-            }
-
-            mdrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    // opens drawer when going back to previous activity
+    @Override
+    public void onBackPressed() {
+        mdrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (mdrawer.isDrawerOpen(GravityCompat.START)) {
             mdrawer.closeDrawer(GravityCompat.START);
-            return true;
-
-
+        } else {
+            super.onBackPressed();
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //functionality to open activities in nav drawer
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            //Handles the click event for the home activity
+            Intent intent = new Intent(this, MainHub_Activity.class);
+            startActivity(intent);
+            return true;
+
+        } else if (id == R.id.nav_new_search) {
+            //Handles the click event for the search activity
+            Intent intent = new Intent(this, SearchFragment.class);
+            startActivity(intent);
+            return true;
+
+            //handles the click even for the saved search activity
+        } else if (id == R.id.nav_saved_searches) {
+            Intent intent = new Intent(this, Saved_Results_Activity.class);
+            startActivity(intent);
+            return true;
+
+            //handles the click even for the forum activity
+        } else if (id == R.id.nav_forum) {
+            Intent intent = new Intent(this, Comments_Rating_Activity.class);
+            startActivity(intent);
+            return true;
+
+            //handles the click even for the user etails activity
+        } else if (id == R.id.nav_user_details) {
+            Intent intent = new Intent(this, MainHub_Activity.class);
+            startActivity(intent);
+            return true;
+
+        }
+
+        mdrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mdrawer.closeDrawer(GravityCompat.START);
+        return true;
+
+
+    }
+
+
+
+}
--- app/src/main/res/layout/Saved_Results_Activity.java
+++ app/src/main/res/layout/Saved_Results_Activity.java
@@ -1,4 +1,3 @@
-
 package randomcompany.trackyourway;
 
 import android.content.Intent;
@@ -11,17 +10,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Comments_Rating_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Saved_Results_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     //creating objects for navigation drawer
     DrawerLayout mdrawer;
@@ -29,16 +19,13 @@ public class Comments_Rating_Activity extends AppCompatActivity implements Navig
     Toolbar toolbar;
 
 
-    private List<Ratings> myRatings = new ArrayList<Ratings>();
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comments__rating_);
-        bar = (RatingBar) findViewById(R.id.ratingBar);
-        populateRatingsList();
-        populateListView();
+        setContentView(R.layout.activity_saved__results_);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -52,16 +39,7 @@ public class Comments_Rating_Activity extends AppCompatActivity implements Navig
         //creating nav view with items in it
         mNavigationView = (NavigationView) findViewById(R.id.nav_view);
         mNavigationView.setNavigationItemSelectedListener(this);
-        
-        //get rating when change is made
-         bar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                System.out.println("The current rating is " + String.valueOf(bar.getRating()));
-            }
-        });
     }
-
 
     // opens drawer when going back to activity
     @Override
@@ -81,6 +59,8 @@ public class Comments_Rating_Activity extends AppCompatActivity implements Navig
         return true;
     }
 
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -96,52 +76,6 @@ public class Comments_Rating_Activity extends AppCompatActivity implements Navig
         return super.onOptionsItemSelected(item);
     }
 
-
-
-
-
-
-
-    private void populateRatingsList() {
-        myRatings.add(new Ratings("Brilliant", R.drawable.stars4));
-        myRatings.add(new Ratings("NCI is the best all the lecturers are great", R.drawable.stars5));
-    }
-
-    private void populateListView() {
-        ArrayAdapter<Ratings> adapter = new MyListAdapter();
-        ListView list = (ListView) findViewById(R.id.ratingListView);
-        list.setAdapter(adapter);
-    }
-
-    private class MyListAdapter extends ArrayAdapter<Ratings>{
-
-        public MyListAdapter(){
-            super(Comments_Rating_Activity.this,R.layout.item_view, myRatings);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent){
-            //Make sure our view works may have passed null
-            View itemView = convertView;
-            if(itemView == null){
-                itemView = getLayoutInflater().inflate(R.layout.item_view, parent, false);
-            }
-
-            //Find The Rating to use
-            Ratings currentRating = myRatings.get(position);
-
-            //fill the list view
-            ImageView imageView = (ImageView)itemView.findViewById(R.id.item_rating_icon);
-            imageView.setImageResource(currentRating.getIconID());
-
-            //Comment
-            TextView commentText = (TextView) itemView.findViewById(R.id.item_comment_txt);
-            commentText.setText(currentRating.getComment());
-
-            return itemView;
-        }
-
-    }
 
     //functionality to open activities in nav drawer
     @SuppressWarnings("StatementWithEmptyBody")
@@ -190,5 +124,13 @@ public class Comments_Rating_Activity extends AppCompatActivity implements Navig
     }
 
 
-}
 
+
+
+
+
+
+
+
+
+}
